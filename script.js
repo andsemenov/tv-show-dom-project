5b1b733fc7941b71aacfcce572b51d3450695287
Andrei <andsemenov@gmail.com>
2020-07-02 19:25:25 +0100

    Added searching functionality

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  //makePageForEpisodes(allEpisodes);
+  ///////////////////////////////////////
+  let counterEpisodes = document.querySelector("#search + label");
+  counterEpisodes.textContent = `Displaying
+  ${allEpisodes.length}/${allEpisodes.length} episodes`;
+  ////////////
+  ////////////
+  document.querySelector("#search").addEventListener("keypress", () => {
+    let stringSearch = document.querySelector("#search").value;
+    document.querySelector("#search + label").textContent = `Displaying ${
+      searchEpisodes(allEpisodes, stringSearch).length
+    }/${allEpisodes.length} episodes`;
+    console.log(searchEpisodes(allEpisodes, stringSearch));
+  });
+
+  ///////////////////////////////////////
+  //const oneShow = getOneShow();
+  //console.log(oneShow);
+  //makePageForEpisodes(oneShow);
 }
 
 function makePageForEpisodes(episodeList) {
@@ -9,4 +27,14 @@ function makePageForEpisodes(episodeList) {
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
 
+//it searches episodes by word
+function searchEpisodes(episodeList, stringForSearch) {
+  let searchingResult = episodeList.filter((episode) => {
+    return (
+      episode.name.includes(stringForSearch) ||
+      episode.summary.includes(stringForSearch)
+    );
+  });
+  return searchingResult;
+}
 window.onload = setup;
