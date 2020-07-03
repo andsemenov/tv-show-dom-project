efccda323a7bccc12e50e27118a9da06a0e76d18
Andrei <andsemenov@gmail.com>
2020-07-03 14:59:27 +0100

    Added rendering of episodes

--- script.js
+++ script.js
@@ -1,40 +1,88 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
-  //makePageForEpisodes(allEpisodes);
-  ///////////////////////////////////////
-  let counterEpisodes = document.querySelector("#search + label");
-  counterEpisodes.textContent = `Displaying
-  ${allEpisodes.length}/${allEpisodes.length} episodes`;
-  ////////////
-  ////////////
-  document.querySelector("#search").addEventListener("keypress", () => {
-    let stringSearch = document.querySelector("#search").value;
-    document.querySelector("#search + label").textContent = `Displaying ${
-      searchEpisodes(allEpisodes, stringSearch).length
-    }/${allEpisodes.length} episodes`;
-    console.log(searchEpisodes(allEpisodes, stringSearch));
-  });
 
-  ///////////////////////////////////////
+  makePageForEpisodes(allEpisodes);
+  searchResultRender(allEpisodes);
   //const oneShow = getOneShow();
   //console.log(oneShow);
   //makePageForEpisodes(oneShow);
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  console.clear(); //////////////////////////////////////////////
+  document.querySelector("#root").innerHTML = "";
+
+  episodeList.forEach(renderEpisode);
+}
+
+// it renders searched episodes and counter of episodes
+function searchResultRender(episodeList) {
+  let counterEpisodes = document.querySelector("#search + label");
+  counterEpisodes.textContent = `Displaying
+  ${episodeList.length}/${episodeList.length} episodes`;
+  document.querySelector("#search").addEventListener("input", () => {
+    let stringSearch = document.querySelector("#search").value;
+    document.querySelector("#search + label").textContent = `Displaying ${
+      searchEpisodes(episodeList, stringSearch).length
+    }/${episodeList.length} episodes`;
+    makePageForEpisodes(searchEpisodes(episodeList, stringSearch));
+  });
 }
 
 //it searches episodes by word
 function searchEpisodes(episodeList, stringForSearch) {
   let searchingResult = episodeList.filter((episode) => {
     return (
-      episode.name.includes(stringForSearch) ||
-      episode.summary.includes(stringForSearch)
+      episode.name.toUpperCase().includes(stringForSearch.toUpperCase()) ||
+      episode.summary.toUpperCase().includes(stringForSearch.toUpperCase())
     );
   });
+
   return searchingResult;
 }
+
+//it makes unique code of each episode
+function settingCode(symbol, number) {
+  let code;
+  if (number < 10) {
+    code = `${symbol}0${number}`;
+  } else {
+    code = `${symbol}${number}`;
+  }
+  return code;
+}
+
+//it renders a container for episode's info
+function renderEpisode(episode) {
+  //nested container for each episode in root div
+  let divContainer = document.createElement("div");
+  document.querySelector("#root").append(divContainer);
+
+  //nested div for title of each episode
+  let divTitle = document.createElement("div");
+  divContainer.append(divTitle);
+
+  //nested name of episode
+  let pName = document.createElement("p");
+  divTitle.append(pName);
+  pName.textContent = episode.name;
+
+  //nested code of episode
+  let pCode = document.createElement("p");
+  divTitle.append(pCode);
+  pCode.textContent =
+    settingCode("s", episode.season) + settingCode("e", episode.number);
+
+  //nested img of each episode
+  let imgMedium = document.createElement("img");
+  divContainer.append(imgMedium);
+  imgMedium.src = episode.image.medium;
+
+  //nested summary paragraph for each episode
+  let divSummary = document.createElement("div");
+  divContainer.append(divSummary);
+  divSummary.innerHTML = episode.summary;
+}
+
 window.onload = setup;
